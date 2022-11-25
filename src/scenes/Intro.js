b3b411b8a40383f75f89e60fcd052136d25046c1
Miguel Lopes <miguelrtlopes@gmail.com>
2022-11-25 16:18:55 -0500

    updated code

--- src/scenes/Intro.js
+++ src/scenes/Intro.js
@@ -14,15 +14,14 @@ export default class Intro extends Phaser.Scene{
 
     create(){
 
-        let bg = this.add.sprite(0,0,'background')
-        bg.setOrigin(0,0)
-
+        this.add.sprite(0,0,'background').setOrigin(0,0)
+       
         let width = this.scale.width
         let height = this.scale.height
 
-        console.log(width)
+        let r1 = this.add.rectangle(width / 6, height / 4, width / 1.5, height / 2, 0xffffff, 90).setOrigin(0,0)
 
-        this.add.text(width / 2, height / 2, 'Hello World', {fontSize: 48})
+        this.add.text(width / 2, height / 2, 'Hello World', {fontSize: 48, color: 0x2127F1}).setOrigin(0.5)
         
     }
 }
