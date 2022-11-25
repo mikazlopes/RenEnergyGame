33f97de38e75cdc0192847edf1ca4f3d1cbfa523
Miguel Lopes <miguelrtlopes@gmail.com>
2022-11-24 20:22:51 -0500

    Added Intro Scene

---
+++ src/scenes/Intro.js
@@ -0,0 +1,17 @@
+import Phaser from '../lib/phaser.js'
+
+export default class Intro extends Phaser.Scene{
+    constructor(){
+    
+        super('intro')
+    
+    }
+
+    preload(){
+
+    }
+
+    create(){
+
+    }
+}
