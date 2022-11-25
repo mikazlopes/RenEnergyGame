44e525434afcc04e88d94a07a623ef811ae939f4
Miguel Lopes <miguelrtlopes@gmail.com>
2022-11-24 21:24:36 -0500

    Added background

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import Intro from './scenes/Intro.js'
 export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 640,
-    height: 480
+    height: 480,
     scene: Intro
     })
 
--- src/scenes/Intro.js
+++ src/scenes/Intro.js
@@ -9,9 +9,20 @@ export default class Intro extends Phaser.Scene{
 
     preload(){
 
+        this.load.image('background', 'assets/background.png')
     }
 
     create(){
 
+        let bg = this.add.sprite(0,0,'background')
+        bg.setOrigin(0,0)
+
+        let width = this.scale.width
+        let height = this.scale.height
+
+        console.log(width)
+
+        this.add.text(width / 2, height / 2, 'Hello World', {fontSize: 48})
+        
     }
 }
