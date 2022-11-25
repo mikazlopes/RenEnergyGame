2b67804b32129fd3c80485fbc892e28671ac3bb1
Miguel Lopes <miguelrtlopes@gmail.com>
2022-11-24 19:34:21 -0500

    Change main.js

--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
 import Phaser from './lib/phaser.js'
-console.log(Phaser)
+
+//Construir Scene para Ecran de entrada
+
+let introScene = new Phaser.Scene('Intro');
+
+let configIntro = {
+    type: Phaser.AUTO,
+    width: 640, 
+    height: 360, 
+    scene: introScene
+};
+
+let introConf = new Phaser.Game(configIntro);
+
+
+
