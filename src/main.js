4cc183fdfb2f3c36905996c82644554a52cdbaef
Miguel Lopes <miguelrtlopes@gmail.com>
2022-11-28 16:05:25 -0500

    criada a cena do mapa

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Phaser from './lib/phaser.js'
 
 import Intro from './scenes/Intro.js'
 
+import Mapa from './scenes/Mapa.js'
+
+
 export default new Phaser.Game({
     type: Phaser.AUTO,
     width: 1280,
     height: 720,
-    scene: Intro
+    scene: Intro, Mapa
     })
 
 
---
+++ src/scenes/Mapa.js
@@ -0,0 +1,19 @@
+import Phaser from '../lib/phaser.js'
+
+
+export default class Mapa extends Phaser.Scene{
+    constructor(){
+    
+        super('mapa')
+    
+    }
+
+    preload(){
+
+    }
+
+    create(){
+
+        
+    }
+}
