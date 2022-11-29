24eef862f7b6ba03322f03d4d24616f7374e0970
Miguel Lopes <miguelrtlopes@gmail.com>
2022-11-29 15:53:16 -0500

    added mapa json

--- src/scenes/Mapa.js
+++ src/scenes/Mapa.js
@@ -10,9 +10,23 @@ export default class Mapa extends Phaser.Scene{
 
     preload(){
 
+        this.load.image('tiles_mapa1', 'assets/tilese/tiles_mapa1.png')
+        this.load.image('tiles_mapa2', 'assets/tilese/tiles_mapa2.png')
+        this.load.image('tiles_mapa3', 'assets/tilese/tiles_mapa3.png')
+        this.load.image('tiles_mapa4', 'assets/tilese/tiles_mapa4.png')
+        this.load.image('tiles_mapa5', 'assets/tilese/tiles_mapa5.png')
+        this.load.image('tiles_mapa6', 'assets/tilese/tiles_mapa6.png')
+        this.load.image('tiles_mapa7', 'assets/tilese/tiles_mapa7.png')
+        
+        this.load.tilemapTiledJSON('mapa', 'assets/tileset/renEnergy_mapa.json')
+
+        this.load.atlas('jack_walk', 'assets/spritesheets/jack_walk_spritesheet.png', 'assets/spritesheets/jack_walk_spritesheet.json')
+
     }
 
     create(){
+
+        
 
         
     }
